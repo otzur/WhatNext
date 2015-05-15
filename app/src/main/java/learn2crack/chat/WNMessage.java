fded3221ca778697e6dce2817ba27b094345e211
otzur <ohadzur10@gmail.com>
2015-05-15 17:28:25 +0300

    few changes

---
+++ app/src/main/java/learn2crack/chat/WNMessage.java
@@ -0,0 +1,15 @@
+package learn2crack.chat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by otzur on 5/9/2015.
+ */
+public class WNMessage  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
